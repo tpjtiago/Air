63cfabca5a6e0dd01c45710de696b97b9862cba1
Tiago Pedro Jr <tpj.tiago@gmail.com>
2020-07-10 00:16:48 -0300

    FluentValidation

---
+++ Air.Domain/Entities/Validations/ClienteValidation.cs
@@ -0,0 +1,13 @@
+﻿using FluentValidation;
+
+namespace Air.Domain.Entities.Validations
+{
+    public class ClienteValidation : AbstractValidator<Cliente>
+    {
+        public ClienteValidation()
+        {
+            RuleFor(x => x.Nome).NotEmpty().WithMessage("O Nome é obrigatório");
+            RuleFor(x => x.Nome).MinimumLength(5).WithMessage("Digite pelo menos 5 letras");
+        }
+    }
+}
