ee7479acb4e7c74ddc614bafa5fd8247b70cd162
Tiago Pedro Jr <tpj.tiago@gmail.com>
2020-07-10 00:35:51 -0300

    DataAnnotations

--- Air.Domain/Entities/Cliente.cs
+++ Air.Domain/Entities/Cliente.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Text;
 
 namespace Air.Domain.Entities
 {
     public class Cliente : BaseEntity
     {
+        [Required(ErrorMessage = "O Nome é Origatório")]
         public string Nome { get; set; }
         public int Idade { get; set; }
     }
